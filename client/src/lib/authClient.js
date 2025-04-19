8390bf3acc21f68364c34c0586ed4af593764351
Steven Yin <syin@groq.com>
2025-04-19 06:20:43 -0400

    fix

--- client/src/lib/authClient.js
+++ client/src/lib/authClient.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/client";
 
 // Create a single instance of the auth client with the correct base URL
 const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_API_URL // Point to your server URL
+  baseURL: import.meta.env.VITE_BETTER_AUTH_URL // Point to your server URL
 });
 
 // Export the singleton instance
